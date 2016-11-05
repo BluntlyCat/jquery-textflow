17a6bc9e6c719aa9fff9fca0129b37bf58c2e952
Michael Jünger <mhaze@mhaze.de>
2016-11-05 03:49:52 +0100

    Fixed path to template

--- django_textflow/templatetags/textflow.py
+++ django_textflow/templatetags/textflow.py
@@ -6,7 +6,7 @@ __author__ = 'mhaze'
 register = template.Library()
 
 
-@register.inclusion_tag('jquery-django_textflow.html')
+@register.inclusion_tag('textflow.html')
 def textflow(texts=None):
     if not isinstance(texts, str):
         raise Exception("Texts must be of type str")
